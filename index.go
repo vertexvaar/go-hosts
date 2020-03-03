414d7c6536845d854728d6ef3d28269b1700589c
Oliver Eglseder <php@vxvr.de>
2020-03-03 21:22:00 +0100

    Accept cli arguments and parse them as ip and domains

--- index.go
+++ index.go
@@ -28,8 +28,41 @@ type HostsEntry struct  {
 	domains []string
 }
 
+func formatIpv4(ip IPv4) (formatted string) {
+	return strconv.Itoa(ip.blk1) + "." + strconv.Itoa(ip.blk2) + "." + strconv.Itoa(ip.blk3) + "." + strconv.Itoa(ip.blk4)
+}
 
 func main() {
+	argsWithoutProg := os.Args[1:]
+	if 1 > len(argsWithoutProg) {
+		fmt.Printf("No arguments given. You must at least provide one IP and one hostname.\n");
+		os.Exit(1);
+	}
+
+	ipRegex := regexp.MustCompile("^(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})\\.(\\d{1,3})$")
+	submatch := ipRegex.FindStringSubmatch(argsWithoutProg[0]);
+
+	if submatch == nil {
+		fmt.Printf("First argument must be an IPv4 address\n");
+		os.Exit(1);
+	}
+
+	blk1, err := strconv.Atoi(submatch[1])
+	check(err)
+	blk2, err := strconv.Atoi(submatch[2])
+	check(err)
+	blk3, err := strconv.Atoi(submatch[3])
+	check(err)
+	blk4, err := strconv.Atoi(submatch[4])
+	check(err)
+
+	entryToAdd := HostsEntry{
+		IPv4{blk1, blk2, blk3, blk4},
+		argsWithoutProg[1:],
+	}
+
+	fmt.Printf("%#v\n", entryToAdd)
+
 	const hostsFile string = "/etc/hosts"
 
 	file, err := os.Open(hostsFile)
@@ -46,7 +79,7 @@ func main() {
 		bytes := scanner.Text()
 		submatch := r.FindAllStringSubmatch(bytes, -1)
 
-		if (submatch == nil) {
+		if submatch == nil {
 			continue
 		}
 
@@ -75,8 +108,4 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func formatIpv4(ip IPv4) (formatted string) {
-	return strconv.Itoa(ip.blk1) + "." + strconv.Itoa(ip.blk2) + "." + strconv.Itoa(ip.blk3) + "." + strconv.Itoa(ip.blk4)
 }
